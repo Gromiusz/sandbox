ccc4e4e3d8fb820e5fde6eb4f24a91584728d666
Gromiusz <kgorka22@gmail.com>
2024-08-14 22:28:34 +0200

    add lab4

--- ccfd_cpp/lab4/lab4-lib.hpp
+++ ccfd_cpp/lab4/lab4-lib.hpp
@@ -1,20 +1,33 @@
 #pragma once
+#include <cstdio>
 
 template<typename T>
 class Pair
 {
 public:
+    Pair(T _f, T _s): first(_f), second(_s) {}
+    Pair() = default; 
     T first;
     T second;
 
     T suma();
 };
 
-// template<typename T, unsigned int N>
-// class PairTable
-// {
+template<typename T, unsigned int N>
+class PairTable
+{
+public:
+    Pair<T> table[N];
 
-// }
+    Pair<T>& operator[](unsigned int num){
+        return table[num];
+    }
+};
+
+template<typename T>
+class PairTable<T, 0>
+{
+};
 
 
 template<typename T>
@@ -22,3 +35,13 @@ T Pair<T>::suma()
 {
     return first + second;
 }
+
+template <typename T>
+struct S {
+    void print() { puts("Szablon ogólny"); }
+};
+
+template <>
+struct S<double> {
+    void print() { puts("Specjalizacja dla double"); }
+};
--- ccfd_cpp/lab4/lab4-main.cpp
+++ ccfd_cpp/lab4/lab4-main.cpp
@@ -1,9 +1,29 @@
 #include "lab4-lib.hpp"
+#include <iostream>
 
 int main()
 {
     int a = 5;
     int c = 2;
     Pair<int> pair;
+    pair.first = 6;
+    pair.second = 3;
+    PairTable<int, 5> table;
+    Pair<int> pair2{3, 4};
+    Pair<int> pair3{1, 2};
 
+    table[0] = pair;
+    table[1] = pair2;
+    table[2] = pair3;
+
+    for(int i = 0; i < 3; i++)
+    {
+        std::cout << table[i].suma() << std::endl;
+    }
+
+    S<int> sint;
+    S<double> sdouble;
+
+    sint.print();
+    sdouble.print();
 }
